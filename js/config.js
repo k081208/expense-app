96de9938ea866bdb7c03d0182eef6afd660ccb36
reino <reino.celeste1220@gmail.com>
2026-07-13 02:41:12 +0900

    撮影のコツをフォームに表示(v5)
    斜めに撮影されたレシート写真ではOCRがほぼ機能しないことが分かったため、
    入力フォームのレシート写真欄に撮影のコツを一言添えた。

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 // 更新のたびに1つずつ増やす。設定画面に表示され、Service Workerのキャッシュ
 // バージョンにも使われるため、更新が実機に反映されたか確認できる
-export const APP_VERSION = '4';
+export const APP_VERSION = '5';
 
 export const SCOPES = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file';
 
